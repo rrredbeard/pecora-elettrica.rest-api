dc15610765383a7467eb5240650082899f09258e
Alessandro Venditti <alessandro.venditti1995@gmail.com>
2019-11-18 00:55:05 +0100

    Added spring app base

---
+++ src/main/java/com/pecora_elettrica/api/PublicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.pecora_elettrica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicLibraryApplication.class, args);
+	}
+
+}
